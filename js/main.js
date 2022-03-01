9a8944586b58245db0d2b60590712ac25d505ce8
Mujakkir85 <mujakkirlucse@gmail.com>
2022-03-01 20:34:12 +0600

    product showed in cart

--- js/main.js
+++ js/main.js
@@ -11,9 +11,11 @@ const loadSearchItem = () => {
 
 const showSearchItems = (items) => {
     console.log(items);
-    if (length === 0) {
-        const errormsg = document.getElementById('error-msg')
-        errormsg.textContent = '';
+
+    // show error for null url
+    const errormsg = document.getElementById('error-msg')
+    errormsg.textContent = '';
+    if (items.length === 0) {
         const div = document.createElement('div');
         div.classList.add('card', 'w-50', 'error-field', 'mx-auto', 'mb-3')
         div.innerHTML = `   
@@ -23,7 +25,33 @@ const showSearchItems = (items) => {
         errormsg.appendChild(div);
     }
 
+    //show items
+    const showItems = document.getElementById('show-items');
+    showItems.textContent = '';
+    let count = 0;
     items.forEach(item => {
         //console.log(item.brand) //brand// phone_name
+        count++;
+        if (count > 20) {
+            return;
+        }
+        else {
+            console.log(count);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div  class="card cart-width shadow p-1 mb-5 bg-body rounded mx-auto" >
+                    <img src="${item.image}" class=" w-50 card-img-top mx-auto" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Name: ${item.brand} </h5>
+                        <h5 class="card-title">Brand: ${item.phone_name} </h5>
+                    </div>
+                    <a href="" class="b-cart mx-auto">Details</a>
+                </div>
+        `
+            showItems.appendChild(div);
+        }
     });
+
+
 }
